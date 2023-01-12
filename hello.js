8d635f23ffa2805674221659af962fcfbab0130c
fanyuebiao <fanyuebiao@zpmc.com>
2023-01-12 21:19:58 +0800

    update hello

--- hello.js
+++ hello.js
@@ -7,3 +7,11 @@ var myName = 'Tom';
 var myAge = 25;
 // 模板字符串
 var sentence = "Hello, my name is ".concat(myName, ".\nI'll be ").concat(myAge + 1, " years old next month.");
+var decLiteral = 6;
+var hexLiteral = 0xf00d;
+// ES6 中的二进制表示法
+var binaryLiteral = 10;
+// ES6 中的八进制表示法
+var octalLiteral = 484;
+var notANumber = NaN;
+var infinityNumber = Infinity;
--- hello.ts
+++ hello.ts
@@ -11,3 +11,12 @@ let myAge: number = 25;
 // 模板字符串
 let sentence: string = `Hello, my name is ${myName}.
 I'll be ${myAge + 1} years old next month.`;
+
+let decLiteral: number = 6;
+let hexLiteral: number = 0xf00d;
+// ES6 中的二进制表示法
+let binaryLiteral: number = 0b1010;
+// ES6 中的八进制表示法
+let octalLiteral: number = 0o744;
+let notANumber: number = NaN;
+let infinityNumber: number = Infinity;
