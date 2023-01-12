d0c2ada4e6e437208dc6d45bf3eaff8a39acdba1
fanyuebiao <fanyuebiao@zpmc.com>
2023-01-12 17:54:16 +0800

    update hello.ts

--- hello.js
+++ hello.js
@@ -3,3 +3,7 @@ function sayHello(person) {
 }
 var user = 'Tom';
 console.log(sayHello(user));
+var myName = 'Tom';
+var myAge = 25;
+// 模板字符串
+var sentence = "Hello, my name is ".concat(myName, ".\nI'll be ").concat(myAge + 1, " years old next month.");
--- hello.ts
+++ hello.ts
@@ -4,3 +4,10 @@ function sayHello(person: string) {
 
 let user = 'Tom';
 console.log(sayHello(user));
+
+let myName: string = 'Tom';
+let myAge: number = 25;
+
+// 模板字符串
+let sentence: string = `Hello, my name is ${myName}.
+I'll be ${myAge + 1} years old next month.`;
