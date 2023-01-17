144bb6c9296f98a9827428292d3121098e3cf0d9
fanyuebiao <fanyuebiao@zpmc.com>
2023-01-17 11:03:40 +0800

    update interface

--- interface.js
+++ interface.js
@@ -2,3 +2,30 @@ var tom = {
     name: 'tom',
     age: 25
 };
+function sum(x, y) {
+    return x + y;
+}
+var mySum = function (x, y) {
+    return x + y;
+};
+function buildName(firstName, lastName) {
+    if (lastName) {
+        return firstName + ' ' + lastName;
+    }
+    else {
+        return firstName;
+    }
+}
+var tomcat = buildName('Tom', 'Cat');
+var tom1 = buildName('Tom');
+var Animal = /** @class */ (function () {
+    function Animal(name) {
+        this.name = name;
+    }
+    Animal.prototype.sayHi = function () {
+        return "My name is ".concat(this.name);
+    };
+    return Animal;
+}());
+var a = new Animal('Jack');
+console.log(a.sayHi()); // My name is Jack
--- interface.ts
+++ interface.ts
@@ -8,3 +8,34 @@ let tom: Person = {
   age: 25
 };
 
+function sum(x, y) {
+  return x + y;
+}
+
+let mySum = function (x, y) {
+  return x + y;
+};
+
+function buildName(firstName: string, lastName?: string) {
+  if (lastName) {
+      return firstName + ' ' + lastName;
+  } else {
+      return firstName;
+  }
+}
+
+let tomcat = buildName('Tom', 'Cat');
+let tom1 = buildName('Tom');
+
+class Animal {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  sayHi(): string {
+    return `My name is ${this.name}`;
+  }
+}
+
+let a: Animal = new Animal('Jack');
+console.log(a.sayHi()); // My name is Jack
